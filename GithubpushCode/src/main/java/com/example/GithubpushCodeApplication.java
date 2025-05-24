273f4336c5ed42554f4115e350197134fd38d81f
vaibhav <dhandevaibhav407@gmail.com>
2025-05-24 04:50:17 -0700

    adde

---
+++ GithubpushCode/src/main/java/com/example/GithubpushCodeApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubpushCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubpushCodeApplication.class, args);
+	}
+
+}
---
+++ GithubpushCode/src/main/java/com/example/controller/Rest.java
@@ -0,0 +1,14 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Rest {
+
+	@GetMapping("train")
+	public String getTrain()
+	{
+		return "getting trainsss...";
+	}
+}
