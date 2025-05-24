6ba2d71bee1d5ef02babca72cb7140fefe4ca000
vaibhav <dhandevaibhav407@gmail.com>
2025-05-24 06:10:09 -0700

    new added

--- GithubpushCode/src/main/java/com/example/controller/Rest.java
+++ GithubpushCode/src/main/java/com/example/controller/Rest.java
@@ -1,6 +1,7 @@
 package com.example.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +11,10 @@ public class Rest {
 	public String getTrain()
 	{
 		return "getting trainsss...";
+	}
+	@PostMapping
+	public String save()
+	{
+		return "save";
 	}
 }
